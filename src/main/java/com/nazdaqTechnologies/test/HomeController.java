fc3610e8eff446d703862450bd5daa8f0ddad3f3
Phillip Ninan <phillip.ninan@nazdaqTechnologies.com>
2013-06-11 18:58:45 -0400

    End of the Day
    Gets parameters from HTTP request. working fine.
    
    IDE is buggy and keeps wants to crash.

--- src/main/java/com/nazdaqTechnologies/test/HomeController.java
+++ src/main/java/com/nazdaqTechnologies/test/HomeController.java
@@ -1,12 +1,13 @@
 package com.nazdaqTechnologies.test;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,32 +21,38 @@ public class HomeController {
 	private static final Logger log = LoggerFactory
 			.getLogger(HomeController.class);
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+
+//	@RequestMapping(value = "/cars", method = RequestMethod.GET)
+//	@ResponseBody
+//	public String handlePost(@RequestParam(value="make",required=false) String make,
+//			@RequestParam(value="model",required=false) String model,
+//			@RequestParam(value="year",required=false) String year, HttpServletRequest request) {
+//
+//		log.debug(request.getParameterMap().keySet().toString());
+//		
+//		Map map = request.getParameterMap();
+//		String make = (String) map.get("make");
+//		return make+" "+model+" "+year;
+//
+//	}
+	@RequestMapping(value = "/cars", method = RequestMethod.GET)
 	@ResponseBody
-	public String home() {
-		log.info("Welcome home! The client locale is {}.");
+	public String handlePost(HttpServletRequest request) {
 
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
-				DateFormat.LONG);
-
-		String formattedDate = dateFormat.format(date);
-
-		// model.addAttribute("serverTime", formattedDate );
-
-		String str = " Hi Naz the date today is " + formattedDate;
-
-		return str;
-	}
-
-	@RequestMapping(value = "/cars/{make}/{year}/{model}", method = RequestMethod.GET)
-	@ResponseBody
-	public String handlePost(@PathVariable String make,@PathVariable String year,@PathVariable String model) {
-
-		return make + " " + year + " " + model;
+		log.debug(request.getParameterMap().keySet().toString());
+		
+		Map map = request.getParameterMap();
+		String year = request.getParameter("year");
+		String model = request.getParameter("model");
+		String make = request.getParameter("make");
+		Iterator it = map.keySet().iterator();
+		while(it.hasNext())
+		{
+			log.debug("Iterator: "+it.next().toString());
+		}
+		
+		//return make+" "+model+" "+year;
+		return it.toString();
 
 	}
 
